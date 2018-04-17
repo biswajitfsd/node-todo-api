78251dcd75a10caeb58ac933120489c296bd9606
bjitpatra1305 <biswajit1305@gmail.com>
2018-04-17 11:54:40 +0530

    Added test case for logout rought

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -298,3 +298,24 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /user/me/token', () => {
+  it('Should remove auth token onlog', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.text).toBe('User loged out successfully.');
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
